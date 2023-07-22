4ef3e196b850143f353550616af1a303ab8575e9
Harsh Gupta <hg242322@gmail.com>
2023-07-22 17:24:59 +0530

    feat: middleware

---
+++ middleware/isAdmin.go
@@ -0,0 +1,74 @@
+package middleware
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"github.com/guptaharsh13/balkanid-task/initializers"
+	"github.com/guptaharsh13/balkanid-task/models"
+	"github.com/guptaharsh13/balkanid-task/utils"
+	"gorm.io/gorm"
+)
+
+func IsAdmin(c *gin.Context) {
+
+	tokenString := c.Request.Header.Get("Authorization")
+	prefix := "Bearer"
+	if strings.HasPrefix(tokenString, prefix) {
+		tokenString = strings.TrimSpace(tokenString[len(prefix):])
+	}
+	if len(tokenString) == 0 {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, utils.UnauthorizedResponse("Authorization token not found"))
+		return
+	}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, utils.UnauthorizedResponse("Invalid token"))
+		return
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		if exp, ok := claims["exp"]; ok {
+			if exp.(float64) < float64(time.Now().Unix()) {
+				c.AbortWithStatusJSON(http.StatusUnauthorized, utils.UnauthorizedResponse("Token expired"))
+				return
+			}
+		}
+		username, ok := claims["username"]
+		if !ok {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, utils.InternalServerErrorResponse())
+			return
+		}
+		var user models.User
+		result := initializers.DB.Take(&user, "username = ?", username)
+		if result.Error != nil {
+			fmt.Printf("User with username %s not found: %s", username, result.Error.Error())
+			c.AbortWithStatusJSON(http.StatusInternalServerError, utils.InternalServerErrorResponse())
+			return
+		}
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, utils.UnauthorizedResponse("Internal Server Error"))
+			return
+		}
+		if !user.IsAdmin {
+			c.AbortWithStatusJSON(http.StatusForbidden, utils.ForbiddenResponse("Forbidden"))
+			return
+		}
+		c.Set("username", user.Username)
+		c.Next()
+	} else {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, utils.UnauthorizedResponse("Invalid token"))
+	}
+}
