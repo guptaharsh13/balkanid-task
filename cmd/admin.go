a1eef37395587bb10856c3c4f3071fa14042fa77
Harsh Gupta <hg242322@gmail.com>
2023-07-22 17:25:51 +0530

    feat: create admin command

--- cmd/admin.go
+++ cmd/admin.go
@@ -1,1 +1,71 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/guptaharsh13/balkanid-task/initializers"
+	"github.com/guptaharsh13/balkanid-task/models"
+	"github.com/guptaharsh13/balkanid-task/utils"
+	"github.com/spf13/cobra"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var adminCommand = &cobra.Command{
+	Use:   "create admin",
+	Short: "This command can be used to create an admin user.",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 3 {
+			fmt.Println("Invalid number of arguments")
+			fmt.Println("Command: create admin <username> <email> <password>")
+			return
+		}
+
+		username := args[0]
+		email := args[1]
+		password := args[2]
+
+		fmt.Println("Creating admin user...")
+		if len(strings.TrimSpace(username)) == 0 {
+			fmt.Println("Username cannot be empty")
+			return
+		}
+		if !utils.IsValidEmail(email) {
+			fmt.Println("Invalid email address")
+			return
+		}
+		if len(strings.TrimSpace(password)) == 0 {
+			fmt.Println("Password cannot be empty")
+			return
+		}
+
+		var user models.User
+		if result := initializers.DB.Take(&user, "username = ?", username); result.RowsAffected > 0 {
+			fmt.Println("Username already taken")
+			return
+		}
+		if result := initializers.DB.Take(&user, "email = ?", username); result.RowsAffected > 0 {
+			fmt.Println("Email already taken")
+			return
+		}
+
+		hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+		if err != nil {
+			fmt.Printf("Couldn't hash password: %s", err.Error())
+			return
+		}
+
+		admin := models.User{
+			Username: username,
+			Email:    email,
+			Password: string(hash),
+			IsActive: true,
+			IsAdmin:  true,
+		}
+		if result := initializers.DB.Create(&admin); result.Error != nil {
+			fmt.Printf("Couldn't create admin user: %s", result.Error.Error())
+			return
+		}
+		fmt.Println("Successfully created admin user")
+	},
+}
--- cmd/main.go
+++ cmd/main.go
@@ -3,34 +3,72 @@ package main
 import (
 	"fmt"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/guptaharsh13/balkanid-task/config"
 	"github.com/guptaharsh13/balkanid-task/controllers"
 	"github.com/guptaharsh13/balkanid-task/initializers"
 	"github.com/guptaharsh13/balkanid-task/routes"
 	"github.com/guptaharsh13/balkanid-task/utils"
+	"github.com/spf13/cobra"
 )
+
+var configuration *config.Config
 
 func init() {
 	initializers.LoadEnvVariables()
 
-	config := config.LoadConfig()
-	initializers.ConnectToDb(config.DB)
+	configuration = config.LoadConfig()
+	initializers.ConnectToDb(configuration.DB)
 	initializers.SyncDatabase()
 	initializers.SyncPermissions()
 	err := utils.SetupValidator()
 	if err != nil {
-		fmt.Println("Couldn't setup validator")
+		fmt.Println("❌ Couldn't setup validator")
 	}
+	fmt.Println("✅ Validator Setup")
+}
+
+func StartApp() error {
+	gin.SetMode(configuration.Environment)
+	fmt.Printf("🚧 Starting %s Environment\n", configuration.Environment)
+
+	r := gin.Default()
+
+	r.Use(cors.Default())
+	if err := r.SetTrustedProxies(configuration.TrustedProxies); err != nil {
+		panic(err)
+	}
+
+	r.GET("/health", controllers.Health)
+	routes.UserRouter(r)
+	routes.TaskRouter(r)
+	routes.GroupRouter(r)
+	routes.RoleRouter(r)
+
+	if err := r.Run(); err != nil {
+		return fmt.Errorf("couldn't start the server: %s", err.Error())
+	} else {
+		fmt.Println("Server started")
+	}
+	return nil
 }
 
 func main() {
-	r := gin.Default()
+	rootCmd := &cobra.Command{
+		Use:   "start",
+		Short: "Start App",
+		Long:  `This start command will start the app.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := StartApp(); err != nil {
+				panic(err)
+			}
+		},
+	}
 
-	r.POST("/signup", controllers.Signup)
-	r.POST("/login", controllers.Login)
+	rootCmd.AddCommand(adminCommand)
 
-	routes.TaskRouter(r)
-
-	r.Run()
+	if err := rootCmd.Execute(); err != nil {
+		panic(err)
+	}
 }
